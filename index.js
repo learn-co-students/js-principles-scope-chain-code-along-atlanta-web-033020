d4ac25782d5daf6d2e94a9baafdc3f7348eaaa49
rpdecks <rpdecks@gmail.com>
2020-05-09 14:40:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-/* Your code goes here */
+var customerName = 'bob';
+const leastFavoriteCustomer = 'mean gene';
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overWriteBestCustomer(newFavorite){
+    bestCustomer = newFavorite;
+}
+
+function changeLeastFavoriteCustomer(){ 
+    leastFavoriteCustomer = 'mean gene'
+} 
+
+function attemptTwoFavoriteCustomers(){
+    let favoriteCustomer = "foo";
+    let favoriteCustomer = "bar";
+}
